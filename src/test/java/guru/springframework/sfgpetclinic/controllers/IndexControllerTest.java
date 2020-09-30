64445f9132f763f872d8bc36f120ad7f676aac98
Tiago Ceccon <ceccon.t@hotmail.com>
2020-09-30 00:33:04 -0300

    Add display name to tests for index controller

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,12 +15,14 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test proper view is returned for index page")
     @Test
     void index() {
         assertEquals("index", controller.index(), "Wrong View Returned");
     }
 
     @Test
+    @DisplayName("Test exception")
     void oupsHandler() {
         assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive Message to build for my test that can be done in a lambda expression");
     }
