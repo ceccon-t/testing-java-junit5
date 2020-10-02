e455d870a431262074bcdc10a3e2984b691f65c5
Tiago Ceccon <ceccon.t@hotmail.com>
2020-10-01 22:51:06 -0300

    Add parameterized test using enum values

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -39,6 +40,13 @@ class OwnerTest implements ModelTests {
     @ValueSource(strings = {"Spring", "Framework", "Guru"})
     void testValueSourcce(String val) {
         System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test ")
+    @ParameterizedTest(name="{displayName} - {{index}} {arguments}")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
     }
 
 }
