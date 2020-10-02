c688e7febc9b29826c3c45a5e23cc12dddb5af06
Tiago Ceccon <ceccon.t@hotmail.com>
2020-10-01 22:55:28 -0300

    Add parameterized test using csv as values

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -35,18 +36,29 @@ class OwnerTest implements ModelTests {
         assertThat(owner.getCity(), is("Key West"));
     }
 
-    @DisplayName("Value Source Test ")
     @ParameterizedTest(name="{displayName} - {{index}} {arguments}")
+    @DisplayName("Value Source Test ")
     @ValueSource(strings = {"Spring", "Framework", "Guru"})
     void testValueSourcce(String val) {
         System.out.println(val);
     }
 
-    @DisplayName("Enum Source Test ")
     @ParameterizedTest(name="{displayName} - {{index}} {arguments}")
+    @DisplayName("Enum Source Test ")
     @EnumSource(OwnerType.class)
     void enumTest(OwnerType ownerType) {
         System.out.println(ownerType);
+    }
+
+    @ParameterizedTest(name="{displayName} - {{index}} {arguments}")
+    @DisplayName("CSV Input Test")
+    @CsvSource({
+        "FL, 1, 1",
+        "OH, 2, 2",
+        "MI, 3, 1"
+    })
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 
 }
